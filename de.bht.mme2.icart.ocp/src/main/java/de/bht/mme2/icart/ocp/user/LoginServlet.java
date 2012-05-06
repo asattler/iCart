ae437c40e8510c6af923826d7f00ccf8f3b3ca72
Andreas Sattler <info@royal-scripts.de>
2012-05-07 01:43:36 +0200

    splitted servlets login implemented

---
+++ de.bht.mme2.icart.ocp/src/main/java/de/bht/mme2/icart/ocp/user/LoginServlet.java
@@ -0,0 +1,74 @@
+package de.bht.mme2.icart.ocp.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class LoginServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private UserDao userDao = new UserDao();
+
+	protected void processRequest(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("application/json");
+		Gson gson = new Gson();
+
+		try {
+			StringBuilder sb = new StringBuilder();
+			String s;
+			while ((s = request.getReader().readLine()) != null) {
+				sb.append(s);
+			}
+			User user = (User) gson.fromJson(sb.toString(), User.class);
+
+			Status status = new Status();
+			if (user.getEmail() != null) {
+				User dbUser = userDao.findByEmail(user.getEmail());
+				if (dbUser != null) {
+					if (dbUser.getPassword().equals(user.getPassword())) {
+						status.setSuccess(true);
+						status.setDescription("success");
+					} else {
+						status.setSuccess(false);
+						status.setDescription("User or Password incorrect.");
+					}
+				} else {
+					status.setSuccess(false);
+					status.setDescription("User or Password incorrect.");
+				}
+			} else {
+				status.setSuccess(false);
+				status.setDescription("error");
+			}
+			response.getOutputStream().print(gson.toJson(status));
+			response.getOutputStream().flush();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			Status status = new Status();
+			status.setSuccess(false);
+			status.setDescription(ex.getMessage());
+			response.getOutputStream().print(gson.toJson(status));
+			response.getOutputStream().flush();
+		}
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request, response);
+	}
+}
---
+++ de.bht.mme2.icart.ocp/src/main/java/de/bht/mme2/icart/ocp/user/RegisterServlet.java
@@ -0,0 +1,71 @@
+package de.bht.mme2.icart.ocp.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class RegisterServlet extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private UserDao userDao = new UserDao();
+
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+        Gson gson = new Gson();
+
+        try {
+            StringBuilder sb = new StringBuilder();
+            String s;
+            while ((s = request.getReader().readLine()) != null) {
+                sb.append(s);
+            }
+            User user = (User) gson.fromJson(sb.toString(), User.class);
+
+            Status status = new Status();
+            if (user.getFirstname() != null) {
+            	if(userDao.findByEmail(user.getEmail()) == null){
+            		this.userDao.save(user);
+            		status.setSuccess(true);
+            		status.setDescription("success");
+            	}
+            	else {
+            		status.setSuccess(false);
+                    status.setDescription("Email already registered.");
+            	}
+            } else {
+                status.setSuccess(false);
+                status.setDescription("error");
+            }
+            response.getOutputStream().print(gson.toJson(status));
+            response.getOutputStream().flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            Status status = new Status();
+            status.setSuccess(false);
+            status.setDescription(ex.getMessage());
+            response.getOutputStream().print(gson.toJson(status));
+            response.getOutputStream().flush();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
